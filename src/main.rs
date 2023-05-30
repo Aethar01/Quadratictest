a98dfdac267846ee2b5261be28582fc36f9f2e41
Aethar <elliott.ashby88@gmail.com>
2023-05-31 00:39:00 +0100

    removed single threading

--- src/main.rs
+++ src/main.rs
@@ -111,20 +111,8 @@ fn main() {
         // quadsfloat.iter().for_each(|q| println!("{:?}", q.get_roots()));
     }
     let elapsed2 = now2.elapsed();
-    let now3 = Instant::now();
-    {
-        quadscom2.into_iter().for_each(|mut q| q.quadratic_eq());
-    }
-    let elapsed3 = now3.elapsed();
-    let now4 = Instant::now();
-    {
-        quadsfloat2.into_iter().for_each(|mut q| q.quadratic_eq());
-    }
     let elapsed4 = now4.elapsed();
     println!("\n Multithreaded:");
     println!("Complex root calculation time elapsed: {:.2?}", elapsed);
     println!("Float root calculation time elapsed: {:.2?}", elapsed2);
-    println!("\n Singlethreaded:");
-    println!("Complex root calculation time elapsed: {:.2?}", elapsed3);
-    println!("Float root calculation time elapsed: {:.2?}", elapsed4);
 }
